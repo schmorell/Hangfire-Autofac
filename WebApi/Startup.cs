da0f7a53f8cc0e1b26f58bbcc2ee16d55abfbc60
Felix Schmorell <felix.schmorell@dialog.ch>
2017-12-22 13:33:34 +0100

    added hangfire, shows already the dashboard under /hangfire

---
+++ WebApi/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Hangfire;
+using Owin;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("DbContext");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
